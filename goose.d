3c1e12fb9d6616f017b359c4890888bd4c7a2ab7
Cronus <cronus@stolenshoe.com>
2025-12-22 11:04:34 -0800

    WIP: package-lock and planning docs

---
+++ goose.d
@@ -0,0 +1,7 @@
+goose.d: crates/goose/src/providers/bedrock.rs
+
+goose: crates/goose/src/providers/bedrock.rs
+
+libgoose.rmeta: crates/goose/src/providers/bedrock.rs
+
+crates/goose/src/providers/bedrock.rs:
